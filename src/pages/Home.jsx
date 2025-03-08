9f032dba83931daa68db1ef61f645ca37b992ac0
LittleBite2 <sanketb790@gmail.com>
2025-03-08 09:46:54 +0530

    animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from '../components/Slider'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  useEffect(() => {
+    AOS.init({
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: true, // whether elements should animate out while scrolling past them
+    });
+    AOS.refresh();
+
+  }, []);
   return (
     <div >
       {/* Slide */}
       <Slider />
 
       {/* our mission */}
+      <div className='w-11/12 mx-auto flex'>
+        <div className='flex'>
+          <img src="./images/home/home-welcome-1.jpg" data-aos="fade-up" alt="" />
+          <img src="./images/home/home-welcome-2.jpg" alt="" />
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi libero quia veritatis obcaecati cum animi dolores rerum delectus sapiente in harum ducimus et, aliquid alias earum perspiciatis iusto possimus quibusdam distinctio unde vitae illo repellendus doloremque? Veritatis aliquid deleniti molestiae voluptas reprehenderit architecto expedita sapiente aperiam aliquam? Totam dolor, pariatur nostrum, dolore in dolorem cupiditate repudiandae temporibus ea ipsam dicta, doloribus assumenda voluptates quo? Quis nihil excepturi quaerat! Neque, soluta repellendus. Temporibus molestiae dolore consectetur vitae optio, pariatur earum, culpa aspernatur, minus magni sed eaque. In, saepe perspiciatis incidunt autem vel, reiciendis facilis dolor illo eum laboriosam aliquid corporis. Ullam?
+        </div>
+      </div>
       <div className='flex flex-col-center '><iframe width="560" height="315" src="https://www.youtube.com/embed/cuIFuOVG9eA?si=7wGHBimrdGVTEDCf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
       <div className='bg-[url(/images/home/bg-building.jpg)] bg-no-repeat bg-cover'>
         <div className=' w-11/12 mx-auto grid grid-cols-2 sm:grid-cols-2 py-2'>
